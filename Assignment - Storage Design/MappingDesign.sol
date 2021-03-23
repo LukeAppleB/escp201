1b9fe628b1ae512d0797017e40847b7e6e3be961
Luke A <lukeappleb@gmail.com>
2021-03-22 21:40:34 -0300

    wip assignment

---
+++ Assignment - Storage Design/MappingDesign.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.8.0;
+
+contract StorageDesign1 {
+   
+    struct Entity {
+        uint data;
+        address _address;
+    }
+    
+    mapping(address => Entity) entities;
+    
+    function addEntity(uint data) public returns (bool success) {
+        entities[msg.sender].data = data;
+        entities[msg.sender]._address = msg.sender;
+        return true;
+    }
+    
+    function updateEntity(uint data) public returns (bool success) {
+        entities[msg.sender].data = data;
+        return true;
+    }
+    
+    function getEntityData() public view returns (uint data) {
+        return entities[msg.sender].data;
+    }
+}
